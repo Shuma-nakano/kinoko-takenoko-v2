c43012e9753885ce88bf64630fc62ed429b7a90c
Shuma-nakano <s1F102500562@iniad.org>
2025-07-02 23:41:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,8 @@ def answer():
         message = re.sub(r'(\d{2,3})-\d+-\d+', r'\1-****-****', message)
         message_html += '<div class="alert {1}" role="alert">{0}</div>\n'.format(
             message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
+        if re.fullmatch(r"https?://\.\w\.\w", message):
+            <a href="https?://\.\w\.\w">https?://\.\w\.\w</a>
 
     return render_template('vote.html', **vars())
 
